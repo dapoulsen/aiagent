c7e042a886c6062644aa53040a2c1399dc264d05
dapoulsen <dapoulsen14@gmail.com>
2025-11-15 12:58:26 +0100

    added better parsing

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
-import sys
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1]
-)
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt")
+parser.add_argument('--verbose', help="show more information", action="store_true")
 
 def main():
 
-    if len(sys.argv) < 2:
-        print("You need to provide a prompt!")
-        sys.exit(1)
+    args = parser.parse_args()
+
+    user_prompt = args.prompt
     
-    print(response.text)
-    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+    
+    if args.verbose:
+
+        print("User prompt: ", user_prompt)
+
+        print(response.text)
+
+        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
+   
     main()
