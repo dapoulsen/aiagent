e961eccf44e7838b432374685f812f99cd9d7fad
dapoulsen <dapoulsen14@gmail.com>
2025-11-25 20:37:42 +0100

    changed system prompt and added func declaraion

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from google.genai import types
 from dotenv import load_dotenv
 import argparse
 from config import SYSTEM_PROMPT
+from call_function import available_functions
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("prompt")
-parser.add_argument('--verbose', help="show more information", action="store_true")
+
 
 def main():
+    load_dotenv()
 
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt")
+    parser.add_argument('--verbose', help="show more information", action="store_true")
     args = parser.parse_args()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
     user_prompt = args.prompt
     
@@ -25,23 +29,34 @@ def main():
     ]
 
 
+    verbose = args.verbose
+
+    if verbose:
+        print(f'User prompt: {user_prompt}\n')
+    
+    generate_content(client, messages, verbose)
+    
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=SYSTEM_PROMPT
+            )
     )
-    
-    if args.verbose:
 
-        print("User prompt: ", user_prompt)
-
-        print(response.text)
-
+   
+    if verbose:
         print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
         print("Response tokens: ", response.usage_metadata.candidates_token_count)
-    else:
-        print(response.text)
 
+    if not response.function_calls:
+        return response.text
+
+    for call in response.function_calls:
+        print(f'Calling function: {call.name}({call.args})')
+ 
 
 if __name__ == "__main__":
    
